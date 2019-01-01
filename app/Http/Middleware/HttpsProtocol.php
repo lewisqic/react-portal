4025b6479fbd59aebf9f3a9b477e109af77200c5
Devin Lewis <devinlewis@gmail.com>
2019-01-01 12:27:22 -0700

    added ssl redirect

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -10,7 +10,7 @@ class HttpsProtocol {
     public function handle($request, Closure $next)
     {
         if ( !$request->secure() ) {
-            //return redirect()->secure($request->getRequestUri());
+            return redirect()->secure($request->getRequestUri());
         }
         return $next($request);
     }
